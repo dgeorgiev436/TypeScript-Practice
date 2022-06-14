0364122d559b9bfb011066e9a4f7a941ecc89e3a
dgeorgiev436 <68106371+dgeorgiev436@users.noreply.github.com>
2022-06-14 15:26:34 +0100

    unknown and never

--- functions.ts
+++ functions.ts
@@ -8,7 +8,6 @@ function printResult(num: number) {
 
 function addAndHandle(n1: number, n2: number, cb: (res: number) => void) {
   const result = n1 + n2;
-  cb(result);
 }
 
 printResult(add(5, 10));
